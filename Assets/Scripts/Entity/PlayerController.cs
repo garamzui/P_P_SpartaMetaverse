634dfc5cd5ce61887affa64075870945de418167
garamzui <forsave121@gmail.com>
2025-05-05 01:17:36 +0900

    Update PlayerController.cs

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,9 +1,36 @@
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
-    protected Rigidbody2D _rigidbody;
-    
-    [SerializeField]private SpriteRenderer chataterRenderer;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
 
+    private float moveX;
+    private float moveY;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // 좌우 입력: A/D 또는 ← →
+        moveX = Input.GetAxisRaw("Horizontal");
+
+        // 상하 입력: W/S 또는 ↑ ↓
+        moveY = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // 양 축 모두 이동 (탑다운 이동)
+        rb.velocity = new Vector2(moveX, moveY).normalized * moveSpeed;
+    }
 }
+
+
+
+
